1124a85d8be445baab42979475cf28af6046f212
soundoffear <m.dyngosz@gmail.com>
2017-12-10 15:31:47 +0400

    Initial commit

---
+++ app/src/main/java/com/ejoapps/m2d2_sub/orderingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ejoapps.m2d2_sub.orderingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
